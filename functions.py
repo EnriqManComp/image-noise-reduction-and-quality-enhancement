04d28714832c5949620f46a867c0e48a839cb838
Enrique Companioni <123147860+EnriqManComp@users.noreply.github.com>
2023-06-08 12:00:17 -0700

    Upload files

---
+++ functions.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import random
+from skimage.filters import gaussian
+
+
+############## NOISE EVENTS ################
+ 
+class add_noise():
+    
+    def __init__(self,image_path):
+        self.image = cv2.imread(image_path)
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+
+    def get_image(self):
+        return self.image        
+        
+    def impulsive(self,num_pixels):
+        # Ruido impulsivo
+        noisy_image = np.copy(self.image)
+        # Generar coordenadas aleatorias para los píxeles afectados por el ruido
+        x_coords = np.random.randint(0, self.image.shape[0], size=num_pixels)
+        y_coords = np.random.randint(0, self.image.shape[1], size=num_pixels)
+        # Asignar valores de sal o pimienta a los píxeles seleccionados
+        for i in range(len(x_coords)):
+            t = np.random.normal(0.5, 0.1)
+            while t < 0 or t > 1:
+                t = np.random.normal(0.5, 0.1)
+            if t <= 0.5:
+                noisy_image[x_coords[i],y_coords[i]] = 0  # Pepper
+            else:
+                noisy_image[x_coords[i],y_coords[i]] = 255  # Sal
+        return noisy_image
+
+    def additive(self,mu,sigma):
+        # Ruido aditivo
+        noise = np.random.normal(mu,sigma,self.image.shape)
+        noisy_image = self.image + noise
+        noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+        return noisy_image
+
+'''class remove_noise():
+
+    def __init__(self,noisy_image):
+        self.noisy_image = noisy_image
+    
+    def remove(self):
+        # Aplanar la imagen en un renglón variacional
+        pass'''
+
+
+
+
+
+
+########################### UNSHARP MASKING ###############################
+
+class unsharp_mask_class():
+
+    def __init__(self):
+        pass
+
+    def unsharp(self,r_image,filtered_image,scale_factor):
+        filtered_image = gaussian(filtered_image,sigma=1,mode='constant',cval=0.0)
+        img = scale_factor*(r_image - filtered_image)
+        unsharp_image = r_image + img
+        return unsharp_image
---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+import functions
+import matplotlib.pyplot as plt
+import skimage
+
+############## MAIN ################
+# Add noise
+image_path = 'PDI/skull14l.jpg'
+noisyObject = functions.add_noise(image_path)
+image = noisyObject.get_image()
+#noisy_image = noisyObject.additive(25,25)
+noisy_image = noisyObject.impulsive(1000)
+
+# Remove noise
+filtered_image = cv2.medianBlur(noisy_image, 3)
+#image_flattened = np.sort(noisy_image.flatten())
+#plt.plot(image_flattened[:],range(len(image_flattened)))
+
+unsharpObject = functions.unsharp_mask_class()
+unsharp_image = unsharpObject.unsharp(r_image=image,filtered_image=filtered_image,scale_factor=1.0)
+
+#plt.show()
+
+
+
+
+
+
+
+############## Visualization #############
+cv2.imshow('Image',image)
+cv2.imshow('Noisy Image',noisy_image)
+cv2.imshow('Filtered Image',filtered_image)
+plt.imshow(unsharp_image, cmap="gray")
+plt.show()
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
